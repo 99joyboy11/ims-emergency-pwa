9ad6ca2d881c8c7d0b64e6509b6926d04ff717e3
99joyboy11 <144240140+99joyboy11@users.noreply.github.com>
2026-02-21 13:15:58 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,20 @@
-const CACHE_NAME = "ims-cache";
+const CACHE_NAME = "ims-cache-v1";
 
 self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache=>{
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/dispatcher.html"
-            ]);
-        })
-    );
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll([
+"/",
+"/index.html",
+"/dispatcher.html"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request)
+.then(res=>res || fetch(e.request))
+);
 });
